3b06d101113eb7359365e2f5039978c2372b8306
권혜주 <2k3m54@naver.com>
2019-09-28 17:58:38 +0900

    Stats 컴포넌트 로직 구현

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,9 @@ import {Stats} from "./Stats";
 export const Header = (props) => {
 	console.log(props);
 	return (
-		<header className="header">
-			<Stats/>
-			<h1 className="h1">{props.title}</h1>
-		</header>
-	);
+	<header className="header">
+		<Stats players={props.players}/>
+		<h1 className="h1">{props.title}</h1>
+	</header>
+);
 }
--- src/Stats.js
+++ src/Stats.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
+// App으로부터 players를 받아서 totalPlayers, totalScore 계산
 export const Stats = (props) => {
+	let totalPlayers = props.players.length;
+	let totalScore = 0;
+	props.players.forEach(player=> {
+		totalScore += player.score;
+	})
+
 	return (
 		<table className="stats">
 			<tbody>
 			<tr>
 				<td>Players:</td>
-				<td>0</td>
+				<td>{totalPlayers}</td>
 			</tr>
 			<tr>
 				<td>Total Points:</td>
-				<td>0</td>
+				<td>{totalScore}</td>
 			</tr>
 			</tbody>
 		</table>
