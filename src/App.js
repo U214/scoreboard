3973bf78f47dff13b4de02fd0502f6e1bdd06b7d
권혜주 <2k3m54@naver.com>
2019-09-28 16:08:50 +0900

    Header 컴포넌트 별도 파일에 분리

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import './App.css';
-
-const Header = (props) => {
-	console.log(props);
-	return (
-		<header className="header">
-			<h1 className="h1">{props.title}</h1>
-			<span className="stats">{props.totalPlayers}</span>
-		</header>
-	);
-}
+import {Header} from "./Header";
 
 const Player = (props) => (
 	<div className="player">
