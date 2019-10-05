18ce874f2fbe0b6b47d0cbb74f138401059daab9
권혜주 <2k3m54@naver.com>
2019-10-05 13:09:34 +0900

    shouldComponent로 렌더링하기

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Counter} from './Counter';
 
-export class Player extends React.PureComponent {
+// pureComponent
+// 내부에서 prop를 비교해줘서 바꼈을때만 렌더링 시켜주게
+// 일일히 shouldComponentUpdate를 구현하지 않아도 되는 것이다
+// 근데 객체를 넘기면 소용이 없다 걔는 레퍼런스타입이니까
+export class Player extends React.Component {
 	render() {
 		console.log(this.props.name, ' rendered');
 		return (
@@ -13,5 +17,9 @@ export class Player extends React.PureComponent {
 				<Counter score={this.props.score} changeScore={this.props.changeScore} id={this.props.id}/>
 				</div>
 		)
+	}
+
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+		return nextProps.score !== this.props.score ? true : false;
 	}
 }
